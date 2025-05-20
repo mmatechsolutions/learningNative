c8713bbc9cb6e1a3ce735ed6e8c2aa8665e5e472
infinitecomm <crissmaina32@gmail.com>
2025-05-20 14:37:04 +0300

    gamezone

--- App.js
+++ App.js
@@ -1,20 +1,33 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React ,{useState}from "react";
+import Home from "./screens/home";
+import * as Font from 'expo-font';
+import AppLoading from 'expo-app-loading';
+
+
+
+const getFonts = () => Font.loadAsync({
+  'nunito-regular': require('./assets/fonts/Nunito-Regular.ttf'),
+  'nunito-bold' : require('./assets/fonts/Nunito-Bold.ttf')
+});
+
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+
+  const [fontsLoaded, setFontsLoaded] = useState(false);
+
+  if (fontsLoaded){
+    return (
+     <Home/>
   );
+  } else{
+    return(
+      <AppLoading 
+      startAsync={getFonts}
+      onFinish ={()=> setFontsLoaded(true)}
+      onError={console.warn}
+      />
+    )
+  };
+  
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
